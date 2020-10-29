b0c10f0d471a77229407a2eae0a5e96850e7393d
Nastya Golubkina <nastya1golubkina@gmail.com>
2020-10-29 13:18:22 +0300

    task factorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -5,4 +5,13 @@
  */
 function factorial(n) {
   // ваш код...
+  let result = 1;
+    for (let i = 0; i < n; i++) {
+      result = result * (n - i);
+    }
+    return result;
 }
+console.log(factorial(0));
+console.log(factorial(1));
+console.log(factorial(3));
+console.log(factorial(5));

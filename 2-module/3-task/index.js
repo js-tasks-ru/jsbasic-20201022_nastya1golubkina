02564cd9564e9dd53147607d804368470e3d14ab
Nastya Golubkina <nastya1golubkina@gmail.com>
2020-11-06 18:14:27 +0300

    calculator v2

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -5,7 +5,7 @@ let calculator = {
     this.a = +prompt('Введите а:', 0);
     this.b = +prompt('Введите b:', 0);
   },
-  
+
   sum() {
     return this.a + this.b;
   },
@@ -16,9 +16,6 @@ let calculator = {
 
 };
 
-calculator.read();
-alert( calculator.sum() );
-alert( calculator.mul() );
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально

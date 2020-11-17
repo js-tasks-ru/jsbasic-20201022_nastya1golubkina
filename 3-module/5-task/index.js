25fdc199f360c02cc7bc03a28840673767c51110
Nastya Golubkina <nastya1golubkina@gmail.com>
2020-11-18 01:22:05 +0300

    getMinMax

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -5,4 +5,12 @@
  */
 function getMinMax(str) {
   // ваш код...
+  let numbersOnly = str
+  .split(',').join(' ').split(' ')
+  .filter(item => Number(item));
+  let result = {
+    min: Math.min(...numbersOnly),
+    max: Math.max(...numbersOnly),
+  }
+  return result;
 }

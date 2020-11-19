4c5a5bb496ae7f9d50303b8315d79e1b5a517940
Nastya Golubkina <nastya1golubkina@gmail.com>
2020-11-19 13:07:38 +0300

    friendsList

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -5,4 +5,9 @@
  */
 function makeFriendsList(friends) {
   // ваш код...
+  const list = document.createElement('UL')
+  for (let name of friends) {
+    list.innerHTML += `<li>${name.firstName} ${name.lastName}</li>`
+  }
+  return list
 }
